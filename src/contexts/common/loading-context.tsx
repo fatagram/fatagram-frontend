b281344c45016644632da075613ce6635548092a
Ngoc Phat <ngocphatc2710@gmail.com>
2025-09-22 21:03:53 +0700

    update UI

---
+++ src/contexts/common/loading-context.tsx
@@ -0,0 +1,48 @@
+import LoadingPage from "@/pages/loading/loading-page";
+import React, { useCallback, useContext, useMemo, useState } from "react";
+
+type LoadingContextType = {
+    increment: () => void;
+    decrement: () => void;
+};
+
+const LoadingContext = React.createContext<LoadingContextType>({
+    increment: () => {},
+    decrement: () => {}
+});
+
+type LoadingProviderProps = {
+    children: React.ReactNode;
+};
+
+export const LoadingProvider: React.FC<LoadingProviderProps> = ({
+    children
+}) => {
+    // const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [count, setCount] = useState(1);
+
+    const stableIncrement = useCallback(() => {
+        setCount(prev => prev + 1);
+    }, []);
+    const stableDecrement = useCallback(() => {
+        setCount(prev => Math.max(0, prev - 1));
+    }, []);
+
+    const value = useMemo(() => ({
+        increment: stableIncrement,
+        decrement: stableDecrement
+    }), [stableIncrement, stableDecrement]);
+
+    return (
+        <LoadingContext.Provider value={value}>
+            {children}
+            {count > 0 && <LoadingPage />}
+        </LoadingContext.Provider>
+    );
+};
+
+export const useLoading = () => {
+    return useContext(LoadingContext);
+};
+
+export default LoadingContext;
