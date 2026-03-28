4d65ba70069f12a66a8fbdfbc7ee7deaf54f8d46
ngocfat <ngocphatc2710@gmail.com>
2026-03-28 19:04:11 +0700

    test: pwa

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "fatagram-cache-v1";
+const ASSETS_TO_CACHE = ["/", "/index.html", "/vite.svg"];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        // optional: update cache
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(event.request)),
+  );
+});
